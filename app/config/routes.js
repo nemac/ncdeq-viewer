4ea90e3eccd9677c5e428fdc8c552081056a92c0
Dave Michelson <daveism@gmail.com>
2016-03-25 12:12:20 -0400

    initial commit fix

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,13 +5,9 @@ var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
-// var Home = require("../components/Home");
-// var PromptContainer = require('../containers/PromptContainer');
-// var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
-// var ResultsContainer = require('../containers/ResultsContainer');
-//
+
 var routes = (
-  <Router history={hashHistory}>
+<Router history={hashHistory}>
     <Route path='/' component={Main}>
     </Route>
   </Router>
--- app/index.js
+++ app/index.js
@@ -1,4 +1,3 @@
-
 var React = require('react');
 var ReactDOM = require('react-dom');
 var routes = require('./config/routes');
