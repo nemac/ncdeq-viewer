d908af079f12bab5ad0044baaea8824516d253ab
Dave Michelson <daveism@gmail.com>
2016-03-31 11:09:03 -0400

    adjust

--- app/components/BreadCrumbWrapper.js
+++ app/components/BreadCrumbWrapper.js
@@ -5,7 +5,26 @@ var BreadCrumbWrapper = React.createClass({
 
   render: function() {
     return (
-      <div />
+      <div className="ui steps">
+        <div className="link step">
+          <div className="content">
+            <div className="title">River Basin</div>
+            <div className="description">The Current River Basin</div>
+          </div>
+        </div>
+        <div className="link step">
+          <div className="content">
+            <div className="title">Cataloging Unit</div>
+            <div className="description">The Current Cataloging Unit</div>
+          </div>
+        </div>
+      <div className="link active step">
+        <div className="content">
+          <div className="title">HUC</div>
+          <div className="description">The Current HUC</div>
+        </div>
+      </div>
+    </div>
     );
   }
 
--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var MainTitle = require('./MainTitle');
+var BreadCrumbWrapper = require('./BreadCrumbWrapper');
 var PropTypes = React.PropTypes;
 
 var Header = React.createClass({
@@ -9,9 +10,7 @@ var Header = React.createClass({
             <div>
               <MainTitle  text='Explore a River Basin'/>
               <p>To get started click a River Basin on the map, or search for a location.</p>
-              {/*
-                bread crumb wrapper - breadcrumbs
-                */}
+              <BreadCrumbWrapper />
             </div>
     );
   }
