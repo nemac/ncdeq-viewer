d55443232abdc930a0f04d628602a08824f9f087
Dave Michelson <daveism@gmail.com>
2016-04-06 08:18:42 -0400

    menu click functionality

--- app/components/BreadCrumb.js
+++ app/components/BreadCrumb.js
@@ -2,26 +2,65 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var BreadCrumb = React.createClass({
-  handleMenuClick: function(e) {
-    console.log(e.target );
-    e.target.className = 'active ' + e.target.className
+  propTypes: {
+    RiverBasinActive: PropTypes.bool.isRequired,
+    CatalogingActive: PropTypes.bool.isRequired,
+    HUCActive: PropTypes.bool.isRequired
+  },
+  getDefaultProps: function() {
+    return {
+      RiverBasinActive: false,
+      CatalogingActive: false,
+      HUCActive: false
+    };
+  },
+  getInitialState: function () {
+    return {
+      RiverBasinActive: false,
+      CatalogingActive: false,
+      HUCActive: false
+    }
+  },
+  resetMenus: function(){
+    //set all to false
+    this.setState({
+      RiverBasinActive: false,
+      CatalogingActive: false,
+      HUCActive: false
+    })
+  },
+  handleMenuClick: function(val,e) {
+
+    //reset menu
+    this.resetMenus();
+
+    //change state to active for clicke menu
+    this.setState({
+      [val]: true,
+    })
+
+
+  },
+  getActive: function(val){
+    return  (this.state[val] ? 'active item' : 'item')
   },
   render: function() {
+
     return (
-      <div className="ui pointing menu" onClick={this.handleMenuClick} >
+      <div className="ui pointing menu"  >
         <div className="header item">
           &nbsp;
         </div>
-        <a className="item">
+        <a className={this.getActive('RiverBasinActive')}  onClick={this.handleMenuClick.bind(this, 'RiverBasinActive')} >
           River Basins
         </a>
-        <a className="item">
+        <a className={this.getActive('CatalogingActive')}  onClick={this.handleMenuClick.bind(this, 'CatalogingActive')} >
           Cataloging Units
         </a>
-        <a className="item">
+        <a className={this.getActive('HUCActive')}  onClick={this.handleMenuClick.bind(this, 'HUCActive')}>
           HUC
         </a>
-        <div className="header item">
+        <div className="header item" >
           &nbsp;
         </div>
         <div className="left menu">
