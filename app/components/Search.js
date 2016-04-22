5b67aba174dd7f13986f66c18a53516532226664
Dave Michelson <daveism@gmail.com>
2016-04-22 09:50:26 -0400

    removed some unused code and components

--- app/components/Search.js
+++ app/components/Search.js
@@ -2,29 +2,11 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 
 var Search = React.createClass({
-  propTypes: {
-    placeholder: PropTypes.string,
-  },
-  getDefaultProps: function() {
-    return {
-      placeholder:'Search...'
-    };
-  },
-  getInitialState: function() {
-    this.placeholder = this.props.placeholder;
-    return {
-      placeholder: this.placeholder
-    };
-  },
   render: function() {
     return (
-            <div className="ui fluid icon input">
-              <input type="text" placeholder={this.state.placeholder} />
-              <i className="inverted circular search link icon"></i>
-            </div>
+      <div />
     );
   }
-
 });
 
 module.exports = Search;
