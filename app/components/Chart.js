7f465efb5d27366a732ba0c6a8da2711aa4b0860
Dave Michelson <daveism@gmail.com>
2016-06-10 09:56:03 -0400

    commit before I move things to redux
    I need the state tree in redux…  too much happening in components. to
    handle state properly…. lesson learned.

--- app/components/Chart.js
+++ app/components/Chart.js
@@ -1,5 +1,10 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var PropTypes = React.PropTypes;
+// require `react-d3-core` for Chart component, which help us build a blank svg and chart title.
+var Chart = require('react-d3-core').Chart;
+// require `react-d3-basic` for Line chart component.
+var LineChart = require('react-d3-basic').LineChart;
 
 var Chart = React.createClass({
 
