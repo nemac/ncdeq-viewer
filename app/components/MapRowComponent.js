fbf71382b8538c4e2c9bdb6b872bedad10a83180
Dave Michelson <daveism@gmail.com>
2016-06-13 11:32:25 -0400

    missed this on combine map row.

--- app/components/MapRowComponent.js
+++ app/components/MapRowComponent.js
@@ -7,44 +7,66 @@ var SearchMapWrapper = require('./SearchMapWrapper');
 
 var PropTypes = React.PropTypes;
 
-function MapRowComponent (props) {
-  return(
-    <div className="ui stackable internally celled grid">
-      <div className="stretched row" >
+var MapRowComponent = React.createClass({
+  componentDidMount: function() {
+    //do something
+  },
+  propTypes: {
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+    zoom: PropTypes.number.isRequired,
+    handleCenter: PropTypes.func.isRequired,
+    handleMapClick: PropTypes.func.isRequired,
+    handleMapMoveEnd: PropTypes.func.isRequired,
+    mapHeight: PropTypes.number.isRequired,
+    rowPadding: PropTypes.number.isRequired
+  },
+  getDefaultProps: function() {
+    return {
+      mapHeight: 300,
+      rowPadding: 1
+    };
+  },
 
-        <MapWrapper HandleMapZoomEnd={props.HandleMapZoomEnd} handleMapMoveEnd={props.handleMapMoveEnd} zoom={props.zoom} latitude={props.latitude} longitude={props.longitude} handleMapClick={props.handleMapClick} rowPadding={props.rowPadding} mapHeight={props.mapHeight} />
+  getInitialState: function () {
+    return {
+      latitude: this.props.latitude,
+      longitude: this.props.longitude,
+      rowPadding: this.props.rowPadding,
+      zoom: this.props.zoom
+    }
+  },
+  render: function() {
+    return (
+      <div className="ui stackable internally celled grid">
+        <div className="stretched row" >
 
-        <div className="four wide column">
+          <MapWrapper HandleMapZoomEnd={this.props.HandleMapZoomEnd} handleMapMoveEnd={this.props.handleMapMoveEnd} zoom={this.props.zoom} latitude={this.props.latitude} longitude={this.props.longitude} handleMapClick={this.props.handleMapClick} rowPadding={this.props.rowPadding} mapHeight={this.props.mapHeight} />
 
-          <div className="ui internally celled grid">
-            <div className="row">
-              <div className="sixteen wide column">
-                <MapLayerToggleWrapper    />
+          <div className="four wide column">
+
+            <div className="ui internally celled grid">
+              <div className="row">
+                <div className="sixteen wide column">
+                  <MapLayerToggleWrapper    />
+                </div>
+              </div>
+              <div className="row">
+                <div className="sixteen wide column">
+                  <ProjectMapWrapper />
+                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="sixteen wide column">
-                <ProjectMapWrapper />
-              </div>
-            </div>
+
           </div>
-
         </div>
+
       </div>
 
-    </div>
-  )
-}
 
-MapRowComponent.PropTypes = {
-  latitude: PropTypes.number.isRequired,
-  longitude: PropTypes.number.isRequired,
-  zoom: PropTypes.number.isRequired,
-  handleCenter: PropTypes.func.isRequired,
-  handleMapClick: PropTypes.func.isRequired,
-  handleMapMoveEnd: PropTypes.func.isRequired,
-  mapHeight: PropTypes.number,
-  rowPadding: PropTypes.number
-}
+    );
+  }
+
+});
 
 module.exports = MapRowComponent;
