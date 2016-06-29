8ec4d01896af72cfe97746f9036ae34915a56041
Dave Michelson <daveism@gmail.com>
2016-06-29 08:40:47 -0400

    router fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import App from '../containers/App';
 import MainComponent from '../components/MainComponent';
 
-import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, hashHistory} from 'react-router';
 
 import { Provider } from 'react-redux';
 import store, {history} from '../stores/RBRPStore';
 
 var routes = (
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={hashHistory}>
       <Route path='/' component={App}>
       </Route>
     </Router>
