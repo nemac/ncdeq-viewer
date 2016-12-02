6c2190faf262c441313a3415fe4b9584110e32cb
Dave Michelson <daveism@gmail.com>
2016-12-02 16:17:35 -0500

    okay have basic construct added to bar charts for clicking on small charts and hover and map

--- app/components/CustomToolTipBarCharts.js
+++ app/components/CustomToolTipBarCharts.js
@@ -40,33 +40,25 @@ const CustomToolTipBarCharts  = React.createClass({
         return HUC12_MAP_FEATUREID
     }
   },
-  // componentDidMount(){
-  //   const { active } = this.props;
-  //   console.log(this.props)
-  //   if (active) {
-  //     const { payload, label } = this.props;
-  //
-  //     const layer_id = this.get_layer_id(this.props.chart_type)
-  //
-  //     $('.recharts-bar-cursor').on("click",function(){console.log('clicked' + JSON.stringify({value:label,chart_type: this.props.chart_type, layer_id: layer_id}))})
-  //   }
-  // },
   componentWillUpdate: function(nextProps, nextState) {
-    // const { payload, labe l } = nextProps.props;
 
-    // console.log(nextProps.payload)
     const layer_id = this.get_layer_id(nextProps.chart_type)
 
+    //yes jquery but I cannot hook to the elements in d3 svg.
+    //  so i need to bind to them...
     $('.recharts-bar-cursor').unbind('click');
-    $('.recharts-bar-rectangles').unbind('click');
     $('.recharts-bar-cursor').unbind('mouseenter');
     $('.recharts-bar-cursor').unbind('mouseleave');
+
+    $('.recharts-bar-rectangles').unbind('click');
+    $('.recharts-bar-rectangles').unbind('mouseenter');
+    $('.recharts-bar-rectangles').unbind('mouseleave');
 
     $('.recharts-bar-cursor').on("click",function(){console.log('clicked' + JSON.stringify({value:nextProps.label,chart_type: nextProps.chart_type, layer_id}))})
     $('.recharts-bar-rectangles').on("click",function(){console.log('clicked' + JSON.stringify({value:nextProps.label,chart_type: nextProps.chart_type, layer_id}))})
 
     $('.recharts-bar-cursor').on("mouseenter",function(){console.log('mouseenter' + JSON.stringify({value:nextProps.label,chart_type: nextProps.chart_type, layer_id}))})
-    $('.recharts-bar-rectangles').on("mouseenter",function(){console.log('clicked' + JSON.stringify({value:nextProps.label,chart_type: nextProps.chart_type, layer_id}))})
+    $('.recharts-rectangle .recharts-bar-rectangles').on("mouseenter",function(){console.log('mouseenter' + JSON.stringify({value:nextProps.label,chart_type: nextProps.chart_type, layer_id}))})
 
     $('.recharts-bar-cursor').on("mouseleave",function(){console.log('mouseleave' + JSON.stringify({value:null,chart_type: null, layer_id: null}))})
     $('.recharts-bar-rectangles').on("mouseleave",function(){console.log('mouseleave' + JSON.stringify({value:null,chart_type: null, layer_id: null}))})
