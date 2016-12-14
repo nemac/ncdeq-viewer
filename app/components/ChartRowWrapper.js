cfed931bcad7ff6fbf25aa5cd2e18a74f7399a17
Dave Michelson <daveism@gmail.com>
2016-12-14 17:04:47 -0500

    separating into components

--- app/components/ChartRowWrapper.js
+++ app/components/ChartRowWrapper.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var ChartBars = require('../components/ChartBars');
+import NoDataMessage from './NoDataMessage'
 
 import {
   BOX_BORDER,
@@ -617,15 +618,7 @@ var ChartRowWrapper = React.createClass({
 
             <div className="description" style={{padding: SPACING,width:this.props.chart_width}}>
             { data.length < 1 &&
-              <div className='ui icon negative message' >
-                <i className="remove circle icon"></i>
-                <div className="content">
-                  <div className="header">
-                    {note}
-                  </div>
-                  {sub_header}
-                </div>
-              </div>
+              <NoDataMessage note={note} sub_header={sub_header} />
             }
             { data.length > 0 &&
               <span>
